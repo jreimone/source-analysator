0a5f74955d497abe03ad286919ddc9ba999fa0ed
Jan <janreimone@gmail.com>
2016-08-28 12:50:56 +0200

    introduced EclipseLibraryFactory

--- product/net.reimone.sourceanalysator.product/src/net/reimone/sourceanalysator/product/E4LifeCycle.java
+++ product/net.reimone.sourceanalysator.product/src/net/reimone/sourceanalysator/product/E4LifeCycle.java
@@ -1,12 +1,6 @@
 package net.reimone.sourceanalysator.product;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import org.eclipse.core.runtime.ILog;
 import org.eclipse.core.runtime.IPath;
@@ -18,17 +12,12 @@ import org.eclipse.e4.ui.workbench.lifecycle.PostContextCreate;
 import org.eclipse.e4.ui.workbench.lifecycle.PreSave;
 import org.eclipse.e4.ui.workbench.lifecycle.ProcessAdditions;
 import org.eclipse.e4.ui.workbench.lifecycle.ProcessRemovals;
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.xmi.XMLResource;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.FrameworkUtil;
 
 import net.reimone.sourceanalysator.Library;
-import net.reimone.sourceanalysator.SourceanalysatorFactory;
+import net.reimone.sourceanalysator.core.ILibraryFactory;
 
 /**
  * This is a stub implementation containing e4 LifeCycle annotated methods.<br />
@@ -43,67 +32,32 @@ public class E4LifeCycle {
 	private Bundle thisBundle;
 
 	@PostContextCreate
-	void postContextCreate(IEclipseContext workbenchContext) {
+	public void postContextCreate(IEclipseContext workbenchContext) {
 	}
 
 	@PreSave
-	void preSave(IEclipseContext workbenchContext) {
+	public void preSave(IEclipseContext workbenchContext) {
 		Library library = workbenchContext.getActive(Library.class);
 		Resource resource = library.eResource();
 		try {
-			resource.save(getProperties());
+			resource.save(EclipseLibraryFactory.getProperties());
 		} catch (IOException e) {
-			log.log(new Status(IStatus.ERROR, thisBundle.getSymbolicName(), "Couldn't create library file", e));
+			log.log(new Status(IStatus.ERROR, thisBundle.getSymbolicName(), "Couldn't save library file", e));
 		}
 	}
 
 	@ProcessAdditions
-	void processAdditions(IEclipseContext workbenchContext) {
+	public void processAdditions(IEclipseContext workbenchContext) {
 		thisBundle = FrameworkUtil.getBundle(getClass());
 		log = Platform.getLog(thisBundle);
 		IPath bundleDataArea = Platform.getStateLocation(thisBundle);
 		IPath libraryPath = bundleDataArea.append(LIBRARY_FILE);
-		ResourceSet rs = new ResourceSetImpl();
-		String libraryPathString = libraryPath.toString();
-		URI uri = URI.createFileURI(libraryPathString);
-		Library library = null;
-		try {
-			// TODO use ILibraryFactory
-			File libraryFile = new File(libraryPathString);
-			if (!libraryFile.exists()) {
-				Resource resource = rs.createResource(uri);
-				library = SourceanalysatorFactory.eINSTANCE.createLibrary();
-				resource.getContents().add(library);
-				resource.save(getProperties());
-			} else {
-				Resource resource = rs.getResource(uri, true);
-				List<EObject> contents = resource.getContents();
-				EObject model = contents.get(0);
-				if (model == null) {
-					library = SourceanalysatorFactory.eINSTANCE.createLibrary();
-					contents.add(library);
-					resource.save(Collections.emptyMap());
-				} else if (!(model instanceof Library)) {
-					library = SourceanalysatorFactory.eINSTANCE.createLibrary();
-					contents.set(0, library);
-					resource.save(Collections.emptyMap());
-				} else {
-					library = (Library) model;
-				}
-			}
-			workbenchContext.set(Library.class, library);
-		} catch (IOException e) {
-			log.log(new Status(IStatus.ERROR, thisBundle.getSymbolicName(), "Couldn't create library file", e));
-		}
-	}
-
-	private Map<Object, Object> getProperties() {
-		Map<Object, Object> properties = new HashMap<>();
-		properties.put(XMLResource.OPTION_ENCODING, StandardCharsets.UTF_8.name());
-		return properties;
+		ILibraryFactory libraryFactory = new EclipseLibraryFactory(libraryPath, log, thisBundle);
+		Library library = libraryFactory.createLibrary();
+		workbenchContext.set(Library.class, library);
 	}
 
 	@ProcessRemovals
-	void processRemovals(IEclipseContext workbenchContext) {
+	public void processRemovals(IEclipseContext workbenchContext) {
 	}
 }
---
+++ product/net.reimone.sourceanalysator.product/src/net/reimone/sourceanalysator/product/EclipseLibraryFactory.java
@@ -0,0 +1,84 @@
+package net.reimone.sourceanalysator.product;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.runtime.ILog;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.osgi.framework.Bundle;
+
+import com.google.common.collect.Maps;
+
+import net.reimone.sourceanalysator.Library;
+import net.reimone.sourceanalysator.SourceanalysatorFactory;
+import net.reimone.sourceanalysator.core.ILibraryFactory;
+
+public class EclipseLibraryFactory implements ILibraryFactory {
+
+	private static final Map<Object, Object> PROPERTIES = Maps.newHashMap();
+	
+	static {
+		PROPERTIES.put(XMLResource.OPTION_ENCODING, StandardCharsets.UTF_8.name());
+	}
+	
+	private IPath libraryPath;
+	private ILog log;
+	private Bundle bundle;
+	
+	public EclipseLibraryFactory(IPath libraryPath, ILog log, Bundle bundle) {
+		this.libraryPath = libraryPath;
+		this.log = log;
+		this.bundle = bundle;
+	}
+
+	@Override
+	public Library createLibrary() {
+		ResourceSet rs = new ResourceSetImpl();
+		String libraryPathString = libraryPath.toString();
+		URI uri = URI.createFileURI(libraryPathString);
+		Library library = null;
+		try {
+			File libraryFile = new File(libraryPathString);
+			if (!libraryFile.exists()) {
+				Resource resource = rs.createResource(uri);
+				library = SourceanalysatorFactory.eINSTANCE.createLibrary();
+				resource.getContents().add(library);
+				resource.save(getProperties());
+			} else {
+				Resource resource = rs.getResource(uri, true);
+				List<EObject> contents = resource.getContents();
+				EObject model = contents.get(0);
+				if (model == null) {
+					library = SourceanalysatorFactory.eINSTANCE.createLibrary();
+					contents.add(library);
+					resource.save(getProperties());
+				} else if (!(model instanceof Library)) {
+					library = SourceanalysatorFactory.eINSTANCE.createLibrary();
+					contents.set(0, library);
+					resource.save(getProperties());
+				} else {
+					library = (Library) model;
+				}
+			}
+		} catch (IOException e) {
+			log.log(new Status(IStatus.ERROR, bundle.getSymbolicName(), "Couldn't load library file", e));
+		}
+		return library;
+	}
+	
+	protected static Map<Object, Object> getProperties() {
+		return PROPERTIES;
+	}
+
+}
