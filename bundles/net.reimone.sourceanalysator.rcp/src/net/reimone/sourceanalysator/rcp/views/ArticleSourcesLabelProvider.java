97e5a8d9c91dd7afaa9572d5dc3be53d286af493
Jan <janreimone@gmail.com>
2016-12-18 11:22:03 +0100

    use generic label provider

---
+++ bundles/net.reimone.sourceanalysator.rcp/src/net/reimone/sourceanalysator/rcp/views/ArticleSourcesLabelProvider.java
@@ -0,0 +1,77 @@
+package net.reimone.sourceanalysator.rcp.views;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import org.eclipse.core.databinding.observable.map.IObservableMap;
+import org.eclipse.emf.ecore.provider.EcoreItemProviderAdapterFactory;
+import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
+import org.eclipse.emf.edit.provider.ReflectiveItemProviderAdapterFactory;
+import org.eclipse.emf.edit.provider.resource.ResourceItemProviderAdapterFactory;
+import org.eclipse.emf.edit.ui.provider.AdapterFactoryLabelProvider;
+import org.eclipse.jface.databinding.viewers.ObservableMapLabelProvider;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+
+import net.reimone.sourceanalysator.GeneralSource;
+import net.reimone.sourceanalysator.Source;
+
+public class ArticleSourcesLabelProvider extends ObservableMapLabelProvider {
+
+	private TableColumn tableColumnGeneralSource;
+	private Table table;
+
+	private AdapterFactoryLabelProvider labelProvider;
+
+	@SuppressWarnings("rawtypes")
+	public ArticleSourcesLabelProvider(IObservableMap[] attributeMaps, TableColumn tableColumnGeneralSource) {
+		super(attributeMaps);
+		this.tableColumnGeneralSource = tableColumnGeneralSource;
+		table = tableColumnGeneralSource.getParent();
+
+		ComposedAdapterFactory adapterFactory = new ComposedAdapterFactory(
+				org.eclipse.emf.edit.provider.ComposedAdapterFactory.Descriptor.Registry.INSTANCE);
+		adapterFactory.addAdapterFactory(new ResourceItemProviderAdapterFactory());
+		adapterFactory.addAdapterFactory(new EcoreItemProviderAdapterFactory());
+		adapterFactory.addAdapterFactory(new ReflectiveItemProviderAdapterFactory());
+		labelProvider = new AdapterFactoryLabelProvider(adapterFactory);
+
+	}
+
+	@Override
+	public Image getColumnImage(Object element, int columnIndex) {
+		return getTextOrImage(element, columnIndex, () -> {
+//			return super.getColumnImage(element, columnIndex);
+			return labelProvider.getImage(element);
+		}, (generalSource) -> {
+			return labelProvider.getImage(generalSource);
+		});
+	}
+
+	@Override
+	public String getColumnText(Object element, int columnIndex) {
+		return getTextOrImage(element, columnIndex, () -> {
+			return super.getColumnText(element, columnIndex);
+		}, (generalSource) -> {
+			String text = labelProvider.getText(generalSource);
+			return text;
+		});
+	}
+
+	private <R> R getTextOrImage(Object element, int columnIndex, Supplier<R> supplier,
+			Function<GeneralSource, R> function) {
+		TableColumn currentColumn = table.getColumn(columnIndex);
+		if (!tableColumnGeneralSource.equals(currentColumn)) {
+			return supplier.get();
+		}
+
+		if (!(element instanceof Source)) {
+			return supplier.get();
+		}
+
+		Source source = (Source) element;
+		GeneralSource generalSource = source.getGeneralSource();
+		return function.apply(generalSource);
+	}
+}
