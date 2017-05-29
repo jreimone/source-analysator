e18e6babd0a3ac8bf3525a11a6d8009be57d5e60
Jan <janreimone@gmail.com>
2017-05-29 13:36:12 +0200

    modified publish script and travis.yml

--- releng/publish.sh
+++ releng/publish.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-# Usage: publish.sh pathToUploadFile package username apikey owner repo version
+# Usage: publish.sh pathToUploadFile package apikey username owner repo version
 API=https://api.bintray.com
 
 function publishRCP() {
 if [ ! -z "$1" ]; then FILE_TO_UPLOAD=$1; fi
 if [ ! -z "$2" ]; then BINTRAY_PCK_NAME=$2; fi
-if [ ! -z "$3" ]; then BINTRAY_USER=$3; fi
-if [ ! -z "$4" ]; then BINTRAY_API_KEY=$4; fi
+if [ ! -z "$3" ]; then BINTRAY_API_KEY=$3; fi
+if [ ! -z "$4" ]; then BINTRAY_USER=$4; fi
 if [ ! -z "$5" ]; then BINTRAY_OWNER=$5; fi
 if [ ! -z "$6" ]; then BINTRAY_REPO=$6; fi
 if [ ! -z "$7" ]; then BINTRAY_PCK_VERSION=$7; fi
+
+TAG=$(git describe --tags --exact-match 2>/dev/null)
+# TAG=${TAG#v}
+
+if [ ! -z "$TAG" ]; then
+  BINTRAY_PCK_VERSION=$TAG
+fi
 
 echo "FILE_TO_UPLOAD=$FILE_TO_UPLOAD"
 echo "BINTRAY_PCK_NAME=$BINTRAY_PCK_NAME"
@@ -17,17 +24,11 @@ echo "BINTRAY_USER=$BINTRAY_USER"
 echo "BINTRAY_OWNER=$BINTRAY_OWNER"
 echo "BINTRAY_REPO=$BINTRAY_REPO"
 echo "BINTRAY_PCK_VERSION=$BINTRAY_PCK_VERSION"
+echo "BINTRAY_API_KEY=$BINTRAY_API_KEY"
 
-TAG=$(git describe --tags --exact-match 2>/dev/null)
-TAG=${TAG#v}
-
-if [ ! -z "$TAG" ]; then
-  BINTRAY_PCK_VERSION=$TAG
-fi
-
-if [ ! -z "$BINTRAY_API_KEY" ]; then
-  BINTRAY_API_KEY=$secure
-fi
+# if [ ! -z "$BINTRAY_API_KEY" ]; then
+#  BINTRAY_API_KEY=$secure
+# fi
 
 #if [ ! -z "$REPOSITORY_PATH" ]; then
 #  pushd "$REPOSITORY_PATH"
@@ -38,24 +39,26 @@ fi
 #FILES=(content.jar artifacts.jar plugins/* features/* binary/*)
 
 BINTRAY_PATH="$BINTRAY_OWNER/$BINTRAY_REPO/$BINTRAY_PCK_NAME/$BINTRAY_PCK_VERSION"
+#BINTRAY_OPTS="bt_package=$BINTRAY_PCK_NAME;bt_version=$BINTRAY_PCK_VERSION;publish=0"
 BINTRAY_OPTS="bt_package=$BINTRAY_PCK_NAME;bt_version=$BINTRAY_PCK_VERSION;publish=0"
 
 # create version
-curl -X POST -u ${BINTRAY_USER}:${BINTRAY_API_KEY} "${API}/packages/$BINTRAY_OWNER/$BINTRAY_REPO/$BINTRAY_PCK_NAME/versions" -d "{ \"name\": \"$BINTRAY_PCK_VERSION\" }" -H "Content-Type: application/json"
+echo "Creating version: $BINTRAY_PCK_VERSION"
+echo curl -X POST -u${BINTRAY_USER}:${BINTRAY_API_KEY} "$API/packages/$BINTRAY_OWNER/$BINTRAY_REPO/$BINTRAY_PCK_NAME/versions" -d "{ \"name\": \"$BINTRAY_PCK_VERSION\" }" -H "Content-Type: application/json"
 
 # upload files
 #for f in ${FILES[@]}
 #do
 #if [ -f $f ]; then
-  echo "Processing file: $FILE_TO_UPLOAD"
-  curl -X PUT -T $FILE_TO_UPLOAD --retry 3 -u ${BINTRAY_USER}:${BINTRAY_API_KEY} "${API}/content/$BINTRAY_PATH/$f;$BINTRAY_OPTS"
+  echo "Uploading file: $FILE_TO_UPLOAD"
+  # curl -X PUT -T $FILE_TO_UPLOAD --retry 3 -u${BINTRAY_USER}:${BINTRAY_API_KEY} "${API}/content/$BINTRAY_PATH/$FILE_TO_UPLOAD;$BINTRAY_OPTS"
+  curl -T $FILE_TO_UPLOAD -u$BINTRAY_USER:$BINTRAY_API_KEY $API/content/$BINTRAY_PATH/$FILE_TO_UPLOAD;$BINTRAY_OPTS
 #fi
 #done
 
 echo "Publishing the new version"
-curl -X POST --retry 3 -u ${BINTRAY_USER}:${BINTRAY_API_KEY} "${API}/content/$BINTRAY_OWNER/$BINTRAY_REPO/$BINTRAY_PCK_NAME/$BINTRAY_PCK_VERSION/publish" -d "{ \"discard\": \"false\" }" -H "Content-Type: application/json"
+curl -X POST --retry 3 -u${BINTRAY_USER}:${BINTRAY_API_KEY} "${API}/content/$BINTRAY_OWNER/$BINTRAY_REPO/$BINTRAY_PCK_NAME/$BINTRAY_PCK_VERSION/publish" -d "{ \"discard\": \"false\" }" -H "Content-Type: application/json"
 
-popd
 }
 
 publishRCP "$@"
